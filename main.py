75b9c320cdb534c6232a94bc9b9b09002fe0efb4
ajrobinson0201 <ajrobinson0201@gmail.com>
2025-09-19 20:39:33 -0400

    UPDATE API

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import os
 import datetime
@@ -13,18 +13,13 @@ CORS(app)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
-@app.route("/api/moisture")
+@app.route("/api/moisture", methods=["POST"])
 def get_moisture():
-    now = datetime.datetime.utcnow()
-    fake_data = [
-        {
-            "timestamp": (now - datetime.timedelta(minutes=i)).isoformat(),
-            "sensor_id": 1,
-            "moisture": round(random.uniform(25.0, 40.0), 2)
-        }
-        for i in range(10)
-    ]
-    return jsonify(fake_data)
+    #now = datetime.datetime.utcnow()
+    data = request.json
+    #sensor_id = data.get("sensor_id")
+    #moisture = data.get("moisture")
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
